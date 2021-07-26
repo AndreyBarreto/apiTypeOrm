f05e0ad395ebefa7c52ac7d99196d68ee47e6f95
AndreyBarreto <andreyvbarreto@gmail.com>
2021-07-26 10:53:13 -0300

    Feature/middleware and .env

---
+++ src/middlewares/adminAuth.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express"
+import { verify } from "jsonwebtoken"
+require('dotenv').config()
+
+
+export function adminAuth(
+    request: Request,
+    response: Response,
+    next: NextFunction
+) {
+    const authToken = request.headers.authorization.split(" ")[1]
+    if (!authToken) {
+        return response.status(401).json({ "message": "Token missing" }).end()
+    }
+    try {
+        const decode = verify(authToken, process.env.SECRET_KEY)
+        console.log(decode)
+    }
+    catch (err) {
+        return response.status(401).json({ "message": "Token invalid" }).end()
+    }
+    next()
+}
